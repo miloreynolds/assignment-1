7a0b20a744f04f3d4106d85194b949cb3217b2dd
Reynolds <reynolds4@holberton.cs.grinnell.edu>
2019-01-28 17:29:31 -0600

    Decode and encode methods completed

--- src/assignment1/CaesarCipher.java
+++ src/assignment1/CaesarCipher.java
@@ -11,9 +11,9 @@ public class CaesarCipher {
       pen.println("Incorrect number of parameters");
       System.exit(1);
     } else if (args[0].equals("encode")) {
-      encode("helloworld");
+      encode(args[1]);
     } else if (args[0].equals("decode")) {
-      decode("Test");
+      decode(args[1]);
     } else {
       pen.println("Valid options are \"encode\" or \"decode\"");
       System.exit(2);
@@ -45,8 +45,24 @@ public class CaesarCipher {
 
   public static void decode(String message) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    pen.println("Decoding");
-    pen.flush();
+    int base = 97;
+    int length = message.length();
+
+    char[] charArray = new char[length];
+
+    for (int n = 0; n < 25; n++) {
+      for (int i = 0; i < length; i++) {
+        int origNum = ((int) message.charAt(i)) - base;
+        
+        int decNum = (origNum + 26 - n) % 26;
+
+        charArray[i] = (char) (decNum + base);
+      }
+
+      String decoded = new String(charArray);
+      pen.println("n = " + n + ": " + decoded);
+      pen.flush();
+    }
   }
 }
 
