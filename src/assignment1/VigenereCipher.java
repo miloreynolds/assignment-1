9ace7ba7ce170aceb0354ad0c2faf62d0950ebb9
Reynolds <reynolds4@licklider.cs.grinnell.edu>
2019-01-29 11:05:23 -0600

    Added comments to VigenereCipher

--- src/assignment1/VigenereCipher.java
+++ src/assignment1/VigenereCipher.java
@@ -1,10 +1,8 @@
 package assignment1;
-
 import java.io.PrintWriter;
 
 public class VigenereCipher {
   public static void main(String[] args) throws Exception {
-
     PrintWriter pen = new PrintWriter(System.err, true);
     if (args.length != 3) {
       pen.println("Incorrect number of parameters");
@@ -18,49 +16,61 @@ public class VigenereCipher {
       System.exit(2);
     }
     pen.flush();
-
-  }
+  }// main(String[])
 
   public static void encode(String message, String key) {
     PrintWriter pen = new PrintWriter(System.out, true);
+    //If key is the empty string, return message as it is
     if (key.equals("")) {
       pen.println(message);
     } else {
       int base = 97;
       int length = message.length();
-
+      
+      //Create character array to store encoded message
       char[] messArray = new char[length];
+      //Modify key to be length of the message
       String wholeKey = new String(createKey(key, length));
-
-
+      
+      //Loop through/decode each character in message
       for (int i = 0; i < length; i++) {
+        //Rebase characters so a=0
         int messNum = ((int) message.charAt(i)) - base;
         int keyNum = ((int) wholeKey.charAt(i)) - base;
+        
+        //Encode character(and wrap around, if necessary)  
         int encNum = (messNum + keyNum) % 26;
+        
+        //Insert into encoded message array
         messArray[i] = (char) (encNum + base);
       }
       String encoded = new String(messArray);
       pen.println(encoded);
     }
     pen.flush();
-  }
-
+  }// encode(String, String)
 
   public static void decode(String message, String key) {
     PrintWriter pen = new PrintWriter(System.out, true);
+  //If key is the empty string, return message as it is
     if (key.equals("")) {
       pen.println(message);
     } else {
       int base = 97;
       int length = message.length();
-
+      
+      //Create character array to store decoded message
       char[] messArray = new char[length];
+      //Modify key to be the length of the message
       String wholeKey = new String(createKey(key, length));
-
-
+      
+      //Loop through/decode each character in message
       for (int i = 0; i < length; i++) {
+        //Rebase characters so a=0
         int messNum = ((int) message.charAt(i)) - base;
         int keyNum = ((int) wholeKey.charAt(i)) - base;
+        
+        //Decode character(and wrap around, if necessary) 
         int decNum = (messNum - keyNum + 26) % 26;
         messArray[i] = (char) (decNum + base);
       }
@@ -68,18 +78,17 @@ public class VigenereCipher {
       pen.println(decoded);
     }
     pen.flush();
-  }
-
+  }// decode (String, String)
 
   public static String createKey(String key, int length) {
+    //Create array to house modified key
     char[] wholeKeyword = new char[length];
-    for (int o = 0; o < length; o++) {
-      wholeKeyword[o] = key.charAt(o % key.length());
-
+    
+    //Repeat keyword until specified length is reached
+    for (int i = 0; i < length; i++) {
+      wholeKeyword[i] = key.charAt(i % key.length());
     }
-
     String wholeKey = new String(wholeKeyword);
     return wholeKey;
-
-  }
-}
+  }// createKey (String, int)
+}// class VigenereCipher
